5b8e6e5d9f69c8f765262c01b6c147210a5bc87f
Reda <muhammadredabh@gmail.com>
2023-01-21 11:18:33 +0100

    3. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_flask(exception):
+    '''storage.close() method to handle'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
