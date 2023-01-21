5f8b8d867bf0894a127a8391735524cc6ea5e225
Reda <muhammadredabh@gmail.com>
2023-01-21 11:42:00 +0100

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,30 @@
 '''API Index view'''
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status')
 def get_status():
     '''status route'''
     return jsonify(status='OK')
+
+@app_views.route('/stats')
+def get_stats():
+    '''stats count route'''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
