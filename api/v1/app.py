9c40994168953ca0d49701e868e0ed6c6d2586d7
Reda <muhammadredabh@gmail.com>
2023-01-21 11:50:54 +0100

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+from flask import jsonify
 
 app = Flask(__name__)
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
@@ -16,6 +17,12 @@ app.register_blueprint(app_views)
 def teardown_flask(exception):
     '''storage.close() method to handle'''
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles 404 HTTP error'''
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == '__main__':
