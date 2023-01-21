4c5f12f400a8b14f072ca046f557ce088f6c90aa
Reda <muhammadredabh@gmail.com>
2023-01-21 11:24:57 +0100

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''API blueprint.'''
 from flask import Blueprint
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''API Index view'''
 from api.v1.views import app_views
 from flask import jsonify
 
